b8103581d38c7e4b21cbad987fd6eee883f2397e
jorfonfir <jorfonfir@alu.edu.gva.es>
2025-05-19 20:53:32 +0000

    Nueva fase deply en pipeline

--- codepipeline.tf
+++ codepipeline.tf
@@ -3,11 +3,20 @@ variable "github_oauth_token" {
   sensitive = true
 }
 
-resource "aws_s3_bucket" "tfvars_bucket" {
-  bucket = "wordpress-terraform-tfvars"
-  force_destroy = true
+# ECR Repository
+resource "aws_ecr_repository" "wordpress" {
+  name                 = "wordpress"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
 }
 
+# S3 Bucket
+resource "aws_s3_bucket" "tfvars_bucket" {
+  bucket         = "wordpress-terraform-tfvars"
+  force_destroy  = true
+}
+
+# IAM Role for CodePipeline
 resource "aws_iam_role" "codepipeline_role" {
   name = "codepipeline-role"
 
@@ -21,32 +30,34 @@ resource "aws_iam_role" "codepipeline_role" {
   })
 }
 
+# IAM Policy for CodePipeline (S3 + CodeBuild access)
 resource "aws_iam_policy" "codepipeline_s3_policy" {
   name = "codepipeline-s3-access"
 
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Effect   = "Allow",
-      Action   = [
-        "s3:GetObject",
-        "s3:GetObjectVersion",
-        "s3:PutObject",
-        "s3:PutObjectAcl",
-        "s3:GetBucketVersioning"
-      ],
-      Resource = [
-        "${aws_s3_bucket.tfvars_bucket.arn}",
-        "${aws_s3_bucket.tfvars_bucket.arn}/*"
-      ]
-    },
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:GetBucketVersioning"
+        ],
+        Resource = [
+          "${aws_s3_bucket.tfvars_bucket.arn}",
+          "${aws_s3_bucket.tfvars_bucket.arn}/*"
+        ]
+      },
       {
         Effect = "Allow",
         Action = [
           "codebuild:StartBuild",
           "codebuild:BatchGetBuilds"
         ],
-        Resource = aws_codebuild_project.terraform_build.arn
+        Resource = aws_codebuild_project.wordpress_image_build.arn
       }
     ]
   })
@@ -57,28 +68,30 @@ resource "aws_iam_role_policy_attachment" "codepipeline_s3_attachment" {
   policy_arn = aws_iam_policy.codepipeline_s3_policy.arn
 }
 
-resource "aws_iam_role" "codebuild_role" {
-  name = "codebuild-terraform-role"
+# IAM Role for CodeBuild
+resource "aws_iam_role" "codebuild_push_ecr_role" {
+  name = "codebuild-push-ecr-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect    = "Allow",
+      Effect = "Allow",
       Principal = { Service = "codebuild.amazonaws.com" },
-      Action    = "sts:AssumeRole"
+      Action = "sts:AssumeRole"
     }]
   })
 }
 
+# Custom IAM Policy for CodeBuild (optional if not using AdministratorAccess)
 resource "aws_iam_policy" "codebuild_policy" {
-  name = "codebuild-terraform-permissions"
+  name = "codebuild-push-ecr-policy"
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "s3:GetObject",
           "s3:GetObjectVersion",
           "s3:PutObject",
@@ -101,11 +114,12 @@ resource "aws_iam_policy" "codebuild_policy" {
       {
         Effect = "Allow",
         Action = [
-          "ec2:*",
+          "ec2:Describe*",
           "ecs:*",
           "elasticloadbalancing:*",
           "iam:PassRole",
-          "cloudwatch:*"
+          "cloudwatch:*",
+          "ecr:*"
         ],
         Resource = "*"
       }
@@ -113,17 +127,18 @@ resource "aws_iam_policy" "codebuild_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild_policy_attachment" {
-  role       = aws_iam_role.codebuild_role.name
+resource "aws_iam_role_policy_attachment" "codebuild_push_ecr_permissions" {
+  role       = aws_iam_role.codebuild_push_ecr_role.name
   policy_arn = aws_iam_policy.codebuild_policy.arn
 }
 
-resource "aws_codebuild_project" "terraform_build" {
-  name        = "terraform-wordpress-build"
-  description = "Terraform deployment"
+# CodeBuild Project for building and pushing Docker image to ECR
+resource "aws_codebuild_project" "wordpress_image_build" {
+  name          = "wordpress-image-build"
+  description   = "Build and push WordPress Docker image to ECR"
   build_timeout = 10
 
-  service_role = aws_iam_role.codebuild_role.arn
+  service_role = aws_iam_role.codebuild_push_ecr_role.arn
 
   artifacts {
     type = "NO_ARTIFACTS"
@@ -133,16 +148,17 @@ resource "aws_codebuild_project" "terraform_build" {
     compute_type    = "BUILD_GENERAL1_SMALL"
     image           = "aws/codebuild/standard:7.0"
     type            = "LINUX_CONTAINER"
-    privileged_mode = false
+    privileged_mode = true
   }
 
   source {
     type      = "GITHUB"
     location  = "https://github.com/jorfonfir/proyecto-fargate.git"
-    buildspec = "buildspec.yml"
+    buildspec = "buildspec.yml" # ✅ Esta línea va aquí
   }
 }
 
+# CodePipeline
 resource "aws_codepipeline" "wordpress_pipeline" {
   name     = "wordpress-pipeline"
   role_arn = aws_iam_role.codepipeline_role.arn
@@ -173,19 +189,38 @@ resource "aws_codepipeline" "wordpress_pipeline" {
   }
 
   stage {
-    name = "Build"
+    name = "DockerBuild"
 
     action {
-      name             = "TerraformBuild"
+      name             = "BuildDockerImage"
       category         = "Build"
       owner            = "AWS"
       provider         = "CodeBuild"
       input_artifacts  = ["source_output"]
-      output_artifacts = []
+      output_artifacts = ["BuildArtifact"]
       version          = "1"
 
       configuration = {
-        ProjectName = aws_codebuild_project.terraform_build.name
+        ProjectName = aws_codebuild_project.wordpress_image_build.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name             = "DeployToECS"
+      category         = "Deploy"
+      owner            = "AWS"
+      provider         = "ECS"
+      input_artifacts  = ["BuildArtifact"]
+      version          = "1"
+
+      configuration = {
+        ClusterName = aws_ecs_cluster.wordpress_cluster.name
+        ServiceName = aws_ecs_service.wordpress_service.name
+        FileName    = "imagedefinitions.json"
       }
     }
   }
